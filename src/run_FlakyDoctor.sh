464711f401569fea24eabc8dcba124e5a9be3b7d
LALAYANG <yangc9@illinois.edu>
2024-07-05 18:46:20 -0500

    add src/run_FlakyDoctor.sh

---
+++ src/run_FlakyDoctor.sh
@@ -0,0 +1,32 @@
+clone_dir=$1
+api_key=$2
+model=$3
+dir_name=$$4
+flakies=$5
+flakiness_type=$6
+nondex_times=5
+
+TimeStamp=$(echo -n $(date "+%Y-%m-%d %H:%M:%S") | shasum | cut -f 1 -d " ")
+
+mkdir -p ${dir_name}
+DIR=${dir_name}/ID_Results_${model}_${clone_dir}_${TimeStamp}
+mkdir -p ${DIR}
+
+
+exec 3>&1 4>&2
+trap $(exec 2>&4 1>&3) 0 1 2 3
+exec 1>${DIR}/${TimeStamp}.log 2>&1
+
+echo "* "STARTING at $(date) 
+echo "* "REPO VERSION $(git rev-parse HEAD)
+# flakies=( od_brit.csv )
+for file in "${flakies[@]}"; do
+    SubTimeStamp=$(echo -n $(date "+%Y-%m-%d %H:%M:%S") | shasum | cut -f 1 -d " ")
+    result_csv=${DIR}/${model}_results_${SubTimeStamp}.csv
+    result_json=${DIR}/${model}_results_${SubTimeStamp}.json
+    save_dir=${DIR}
+    details_json=${DIR}/${model}_test_final_result_${SubTimeStamp}.json
+    python3 -u src/flakydoctor.py --input-tests-csv ${file} --projects ${clone_dir} --openai-key ${api_key} --model ${model} --nondex-times ${nondex_times} --output-dir ${save_dir} --output-result-csv ${result_csv} --output-result-json ${result_json} --output-details-json ${details_json} --flakiness-type ${flakiness_type}
+done
+
+echo "* "ENDING at $(date)
