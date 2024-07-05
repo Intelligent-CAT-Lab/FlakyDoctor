7e82d2abe76613ea54f5765636ccee20330b32dd
LALAYANG <yangc9@illinois.edu>
2024-07-05 13:33:34 -0500

    update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -6,9 +6,9 @@ apt-get install -y openjdk-11-jdk
 apt-get install autoconf -y
 apt-get install libtool -y
 apt install protobuf-compiler -y
-
 add-apt-repository ppa:deadsnakes/ppa -y
 apt install git-all -y
+
 pip3 install jsonlines
 pip3 install GitPython
 pip3 install beautifulsoup4==4.12.3
@@ -16,8 +16,9 @@ pip3 install javalang==0.13.0
 pip3 install openai==0.28.0
 pip3 install torch==2.1.0
 pip3 install transformers==4.31.0
-pip3 install git+https://github.com/jose/javalang.git@start_position_and_end_position
 pip3 install accelerate==0.21.0
+pip3 install git+https://github.com/jose/javalang.git@start_position_and_end_position
+
 
 wget https://github.com/google/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz
 tar xvf protobuf-2.5.0.tar.gz
